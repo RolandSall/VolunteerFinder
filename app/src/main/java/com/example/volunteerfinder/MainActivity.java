4a8fe9d8e5f574d55203c44777a1bb6f24fafceb
Roland Salloum <roland.salloum00@outlook.com>
2020-11-29 17:00:21 +0200

    feat: added UI part for register

--- app/src/main/java/com/example/volunteerfinder/MainActivity.java
+++ app/src/main/java/com/example/volunteerfinder/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.volunteerfinder;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
