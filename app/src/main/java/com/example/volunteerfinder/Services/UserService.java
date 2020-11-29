b11826cdacb4a05876cf64ab5c069662dd61ec26
Roland Salloum <roland.salloum00@outlook.com>
2020-11-29 20:25:41 +0200

    feat: added hashing for password

--- app/src/main/java/com/example/volunteerfinder/Services/UserService.java
+++ app/src/main/java/com/example/volunteerfinder/Services/UserService.java
@@ -5,6 +5,8 @@ import com.example.volunteerfinder.model.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 public class UserService implements IUserService {
@@ -15,9 +17,30 @@ public class UserService implements IUserService {
 
     @Override
     public void save(User request) {
-        UUID uuid=UUID.randomUUID();
+        UUID uuid = UUID.randomUUID();
+        String hashPassword = generateHash(request.getPassword());
+        request.setPassword(hashPassword);
         reference.child(uuid.toString()).setValue(request);
         System.out.println(request);
 
+    }
+
+    public static String generateHash(String input) {
+        StringBuilder hash = new StringBuilder();
+
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            byte[] hashedBytes = sha.digest(input.getBytes());
+            char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'a', 'b', 'c', 'd', 'e', 'f'};
+            for (int idx = 0; idx < hashedBytes.length; idx++) {
+                byte b = hashedBytes[idx];
+                hash.append(digits[(b & 0xf0) >> 4]);
+                hash.append(digits[b & 0x0f]);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            // handle error here.
+        }
+        return hash.toString();
     }
 }
