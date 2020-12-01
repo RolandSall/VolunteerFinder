c44f748724d980bb2bab0e78370f9463c693e97d
Boudi <51781491+abdallah75@users.noreply.github.com>
2020-12-01 20:55:00 +0200

    fix: adjust zoom on map

--- app/src/main/java/com/example/volunteerfinder/activities/MapsActivity.java
+++ app/src/main/java/com/example/volunteerfinder/activities/MapsActivity.java
@@ -44,8 +44,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
 
             mMap.addMarker(new MarkerOptions().position(latLng).title("Event Location"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 9));
         }
         catch (IOException e){
             System.out.println("Location Not Found");
