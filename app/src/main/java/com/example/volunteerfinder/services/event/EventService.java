39e6bf75109f7beefad0ee5d051f6cf65ca74b79
Roland Salloum <roland.salloum00@outlook.com>
2020-12-03 23:16:46 +0200

    refactor: removed dummy post event in EventService

--- app/src/main/java/com/example/volunteerfinder/services/event/EventService.java
+++ app/src/main/java/com/example/volunteerfinder/services/event/EventService.java
@@ -53,50 +53,6 @@ public class EventService implements IEventService {
         dbReference.child(eventId).removeValue();
     }
 
-    private Event createDummyEvent(UUID uuid) {
-        ArrayList participants = new ArrayList();
-        participants.add("1");
-        participants.add("2");
-        participants.add("3");
-        return Event.builder()
-                .eventId(uuid.toString())
-                .capacity(10)
-                .organization(new Organization().builder()
-                        .organizationId(UUID.randomUUID().toString())
-                        .webPage("https://www.lau.edu.lb/")
-                        .name("Dummy Organization")
-                        .build())
-                .description("Help Students")
-                .location("Hamra")
-                .title("Event Title")
-                .image("https://firebasestorage.googleapis.com/v0/b/volunteerfinder-8df78.appspot.com/o/Events%2F6ccdec15-2343-4666-af38-8fd664e39171%2Fmlops-1.png?alt=media&token=ab1513f4-e874-4b01-92d3-4e71b361e86e")
-                .eventDate("12-25-2020")
-                .postedDate("11-30-2020")
-                .participants(participants)
-                .build();
-    }
-
-    public Event createDummyEvent(Organization organization) {
-        UUID uuid = UUID.randomUUID();
-        ArrayList participants = new ArrayList();
-        participants.add("1");
-        participants.add("2");
-        participants.add("3");
-        return Event.builder()
-                .eventId(uuid.toString())
-                .capacity(10)
-                .organization(organization)
-                .description("Help Students")
-                .location("Hamra")
-                .title("Event Title")
-                .image("https://firebasestorage.googleapis.com/v0/b/volunteerfinder-8df78.appspot.com/o/Events%2F6ccdec15-2343-4666-af38-8fd664e39171%2Fmlops-1.png?alt=media&token=ab1513f4-e874-4b01-92d3-4e71b361e86e")
-                .eventDate("12-25-2020")
-                .postedDate("11-30-2020")
-                .participants(participants)
-                .build();
-    }
-
-
 
     private List<Event> buildListOfEvents(List<EventsServiceResponse> eventsServiceResponses) {
         while (eventsServiceResponses.equals(null)){
