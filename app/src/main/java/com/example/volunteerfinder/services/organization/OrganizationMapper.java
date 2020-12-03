023faeded607fa2d5434907d9f83d6b2a1ca3d5c
Roland Salloum <roland.salloum00@outlook.com>
2020-12-04 01:06:13 +0200

    refactor: hashing service

--- app/src/main/java/com/example/volunteerfinder/services/organization/OrganizationMapper.java
+++ app/src/main/java/com/example/volunteerfinder/services/organization/OrganizationMapper.java
@@ -12,6 +12,7 @@ public class OrganizationMapper {
                 .name(organizationHashMap.get("name").toString())
                 .webPage(organizationHashMap.get("webPage").toString())
                 .organizationId(organizationHashMap.get("organizationId").toString())
+                .organizationId(organizationHashMap.get("address").toString())
                 .build();
     }
 }
--- app/src/main/java/com/example/volunteerfinder/services/user/UserService.java
+++ app/src/main/java/com/example/volunteerfinder/services/user/UserService.java
@@ -1,6 +1,7 @@
 package com.example.volunteerfinder.services.user;
 
 import com.example.volunteerfinder.models.User;
+import com.example.volunteerfinder.services.helper.HashingService;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,13 +12,14 @@ import java.util.UUID;
 public class UserService implements IUserService {
 
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference dbReference = database.getReference("Users");
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbReference = database.getReference("Users");
+    private HashingService hashingService = new HashingService();
 
     @Override
     public RegisterUserResponse save(User request) throws Exception {
         UUID uuid = UUID.randomUUID();
-        String hashPassword = generateHash(request.getPassword());
+        String hashPassword = hashingService.generateHash(request.getPassword());
         request.setPassword(hashPassword);
         dbReference.child(uuid.toString()).setValue(request);
         RegisterUserResponse response = buildServiceResponseFromFireBaseResponse(uuid, request);
@@ -35,22 +37,4 @@ public class UserService implements IUserService {
                 .build();
     }
 
-    public static String generateHash(String input) throws Exception {
-        StringBuilder hash = new StringBuilder();
-
-        try {
-            MessageDigest sha = MessageDigest.getInstance("SHA-1");
-            byte[] hashedBytes = sha.digest(input.getBytes());
-            char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                    'a', 'b', 'c', 'd', 'e', 'f'};
-            for (int idx = 0; idx < hashedBytes.length; idx++) {
-                byte b = hashedBytes[idx];
-                hash.append(digits[(b & 0xf0) >> 4]);
-                hash.append(digits[b & 0x0f]);
-            }
-        } catch (NoSuchAlgorithmException e) {
-            throw new Exception("Failed To Save User");
-        }
-        return hash.toString();
-    }
 }
