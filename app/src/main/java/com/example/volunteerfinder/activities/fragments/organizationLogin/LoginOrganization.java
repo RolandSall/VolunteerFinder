71976f00f53c3b11127c691c7be2b38f771c9d2b
Boudi <51781491+abdallah75@users.noreply.github.com>
2020-12-05 13:25:49 +0200

    fix: organization login fragment ref

--- app/src/main/java/com/example/volunteerfinder/activities/fragments/organizationLogin/LoginOrganization.java
+++ app/src/main/java/com/example/volunteerfinder/activities/fragments/organizationLogin/LoginOrganization.java
@@ -40,7 +40,7 @@ public class LoginOrganization extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_login_organization, container, false);
     }
 
     @Override
--- app/src/main/java/com/example/volunteerfinder/activities/fragments/organizationLogin/SignUpOrganization.java
+++ app/src/main/java/com/example/volunteerfinder/activities/fragments/organizationLogin/SignUpOrganization.java
@@ -48,7 +48,7 @@ public class SignUpOrganization extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        return inflater.inflate(R.layout.fragment_signup_organization, container, false);
     }
 
     @Override
