f871e50d446d190d2d85176828baf597a6e74f3b
HadiKhai <hadikhairallah@outlook.com>
2020-12-05 15:26:27 +0200

    feat: add event in organization

--- app/src/main/java/com/example/volunteerfinder/activities/OrganizationProfileActivity.java
+++ app/src/main/java/com/example/volunteerfinder/activities/OrganizationProfileActivity.java
@@ -10,7 +10,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.volunteerfinder.R;
 import com.example.volunteerfinder.adapters.EventAdapter;
@@ -18,6 +20,7 @@ import com.example.volunteerfinder.models.Event;
 import com.example.volunteerfinder.models.Organization;
 import com.example.volunteerfinder.models.User;
 import com.example.volunteerfinder.services.event.EventService;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -35,6 +38,9 @@ public class OrganizationProfileActivity extends AppCompatActivity implements Ev
 
     private Organization organization;
 
+    FloatingActionButton fab1, fab2;
+    View fabBGLayout;
+    boolean isFABOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +52,10 @@ public class OrganizationProfileActivity extends AppCompatActivity implements Ev
     private void initSetup() {
         eventList = new ArrayList<>();
         eventRecyclerView = findViewById(R.id.eventRecyclerView);
-        addEventButton = findViewById(R.id.addEventButton);
 
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        fabBGLayout = findViewById(R.id.fabBGLayout);
         sp = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
         organization = new Gson().fromJson(sp.getString("organization", ""), Organization.class);;
 
@@ -65,6 +73,41 @@ public class OrganizationProfileActivity extends AppCompatActivity implements Ev
             intent.putExtra("organization", organization);
             startActivity(intent);
         });
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFABOpen) {
+                    showFABMenu();
+                } else {
+                    closeFABMenu();
+                }
+            }
+        });
+
+        fabBGLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeFABMenu();
+            }
+        });
+    }
+
+    private void showFABMenu() {
+        isFABOpen = true;
+        fabBGLayout.setVisibility(View.GONE);
+        fab1.animate().rotationBy(180);
+        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_100));
+        fab2.setVisibility(View.GONE);
+    }
+
+    private void closeFABMenu() {
+        isFABOpen = false;
+        fabBGLayout.setVisibility(View.GONE);
+        fab1.animate().rotation(0);
+        fab2.setVisibility(View.INVISIBLE);
+        fab2.animate().translationY(0);
     }
 
     @Override
